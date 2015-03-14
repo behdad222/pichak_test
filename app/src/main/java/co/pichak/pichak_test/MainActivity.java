37878f556aea9c2b5896c2b475daa617f4a85690
behdad222 <behdad.222@gmail.com>
2015-03-14 14:21:18 +0330

    add counter

--- app/src/main/java/co/pichak/pichak_test/MainActivity.java
+++ app/src/main/java/co/pichak/pichak_test/MainActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 
+import co.pichak.pichak_test.View.CustomView.CircleTextView;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     private Button start;
     private Button stop;
+    private CircleTextView counter;
+    private int count;
+    private boolean startStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +20,14 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         start = (Button) findViewById(R.id.start);
         stop = (Button) findViewById(R.id.stop);
+        counter = (CircleTextView) findViewById(R.id.counter);
 
         start.setOnClickListener(this);
         stop.setOnClickListener(this);
+        counter.setOnClickListener(this);
+
+        count = 0;
+        startStatus = false;
     }
 
     @Override
@@ -27,11 +36,22 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             case R.id.start:
                 stop.setVisibility(View.VISIBLE);
                 start.setVisibility(View.INVISIBLE);
+                startStatus = true;
                 break;
 
             case R.id.stop:
                 start.setVisibility(View.VISIBLE);
                 stop.setVisibility(View.INVISIBLE);
+                startStatus = false;
+                count = 0;
+                counter.setText(String.valueOf(count));
+                break;
+
+            case R.id.counter:
+                if (startStatus) {
+                    count++;
+                    counter.setText(String.valueOf(count));
+                }
                 break;
         }
     }
